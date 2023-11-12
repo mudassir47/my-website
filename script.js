84fd5ab2d7122f873f504ad5bcf914fd20320b91
mudassir47 <mudassirs472@gmail.com>
2023-11-12 14:22:40 +0530

    publish

---
+++ script.js
@@ -0,0 +1,11 @@
+
+  
+  // Initialize Firebase
+  const firebaseApp = initializeApp(firebaseConfig);
+
+  // Reference to the database
+  const database = getDatabase(firebaseApp);
+  const btn = document.querySelector('.button');
+  function submitForm() {
+  btn.style
+  }
